064435e10e895fb801b6ae4d330483fee5449c29
Michael Quarne <mikedq1225@gmail.com>
2020-07-14 18:02:08 -0700

    win and lose are working

---
+++ app.js
@@ -0,0 +1,60 @@
+// Game values
+let min = 1
+let max = 10
+let winningNum = 2
+let guessesLeft = 3
+
+// UI Elements
+const game = document.getElementById("game")
+const minNum = document.querySelector(".min-num")
+const maxNum = document.querySelector(".max-num")
+const guessBtn = document.querySelector("#guess-btn")
+const guessInput = document.querySelector("#guess-input")
+const message = document.querySelector(".message")
+
+// UI min and max numbers
+minNum.textContent = min
+maxNum.textContent = max
+
+// Button event listener
+guessBtn.addEventListener("click", function (e) {
+	let guess = parseInt(guessInput.value)
+	// Validate input
+	if (isNaN(guess) || guess < min || guess > max) {
+		setMessage(`Please enter a number between ${min} and ${max}`, "red")
+	}
+
+	// Check if winning number
+	if (guess === winningNum) {
+		// Game over - win
+		gameOver(true, `Congratulations, ${winningNum} is correct.  You Win!`)
+	} else {
+		guessesLeft -= 1
+		if (guessesLeft === 0) {
+			// Game Over - lose
+			gameOver(false, `Sorry, You Lose.  ${winningNum} was the correct answer.`)
+		} else {
+			// Game continues - wrong answer
+			guessInput.value = "" // Clears input
+			setMessage(
+				`${guess} was not correct.  You have ${guessesLeft} guesses left`,
+				"red",
+				"red"
+			)
+		}
+	}
+})
+
+function gameOver(win, msg) {
+	let color
+	win === true ? (color = "green") : (color = "red") // Sets color based on win(true) or lose(false)
+	guessInput.disabled = true // Disables input
+	guessInput.style.borderColor = color // Changes border color
+	message.style.color = color // Changes text color
+	setMessage(msg) // Sets message to what is passed in
+}
+
+function setMessage(msg, color) {
+	message.style.color = color
+	message.textContent = msg
+}
